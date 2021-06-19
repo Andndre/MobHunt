3f447542d88f69563ebbd06191065ed6a6f8886f
AgungAndre01 <agungandre687@gmail.com>
2021-06-19 11:06:56 +0800

    Initial commit

---
+++ src/me/Andre/MobHunt/ItemHandler.java
@@ -0,0 +1,56 @@
+package me.Andre.MobHunt;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.Arrays;
+import java.util.Random;
+
+@SuppressWarnings("ALL")
+public class ItemHandler {
+    private static final Random random = new Random();
+
+
+    public static boolean isHolding(ItemStack itemStack, Player player){
+        PlayerInventory inventory = player.getInventory();
+        if(inventory.getItemInMainHand().isSimilar(itemStack)){
+            return true;
+        }
+        return inventory.getItemInOffHand().isSimilar(itemStack);
+    }
+
+    public static ItemStack getRandomDiamondItems(){
+        Material mat = Material.values()[random.nextInt(Material.values().length)];
+
+        while (!mat.name().contains("DIAMOND")){
+            mat = Material.values()[random.nextInt(Material.values().length)];
+        }
+
+        ItemStack result = new ItemStack(mat, 1);
+        ItemMeta meta = result.getItemMeta();
+        meta.addEnchant(Enchantment.LUCK, 1, true);
+        meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+        result.setItemMeta(meta);
+
+        return result;
+    }
+
+    public static ItemStack getSpawnBasket(){
+        ItemStack spawnBasket;
+        spawnBasket = new ItemStack(Material.DIAMOND_SHOVEL, 1);
+        ItemMeta meta  = spawnBasket.getItemMeta();
+        meta.setDisplayName("Basket Spawner");
+        meta.setLore(Arrays.asList(ChatColor.GREEN + "Left Click on ground Spawn your Basket", "You can only use this once"));
+        meta.addEnchant(Enchantment.LUCK, 1, true);
+        meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+        spawnBasket.setItemMeta(meta);
+
+        return spawnBasket;
+    }
+}
---
+++ src/me/Andre/MobHunt/Main.java
@@ -0,0 +1,447 @@
+package me.Andre.MobHunt;
+
+import me.Andre.API.ConfigManager;
+import me.Andre.API.HashMapHelper;
+import me.Andre.API.ScoreboardManager;
+import org.bukkit.*;
+import org.bukkit.block.Block;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.*;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.EntityPickupItemEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitScheduler;
+import org.bukkit.util.Vector;
+
+import java.util.*;
+
+@SuppressWarnings({"unused", "ConstantConditions"})
+public class Main extends JavaPlugin implements Listener {
+
+    private ConfigManager settings;
+    private HashMapHelper hashMapHelper;
+    private ScoreboardManager scoreboard;
+    private long delay;
+    private Map<Player, List<EntityType>> collectedType;
+    private Map<String, Integer> points;
+    private Map<Player, Integer> tasks;
+    private Map<Player, List<Location>> locsInBasket;
+    private Map<Player, Location> basketLocs;
+    private BukkitScheduler scheduler;
+    private List<Item> items;
+
+
+    public void increment(Map<String, Integer> map, String player, Integer addition){
+        if(map.containsKey(player)){
+            map.replace(player, map.get(player) + addition);
+        }
+        else {
+            map.put(player, addition);
+        }
+    }
+
+    public void countDown(){
+        scheduler.scheduleSyncRepeatingTask(this, new Runnable() {
+            int count = 10;
+            @Override
+            public void run() {
+                getServer().broadcastMessage(ChatColor.RED + "time left: " + count + " seconds");
+                count--;
+                if(count < 0){
+                    stopGame();
+                }
+            }
+        }, 0L, 20L);
+    }
+
+    public void msgPoints(){
+        for(Map.Entry<String, Integer> point: points.entrySet()){
+            this.getServer().broadcastMessage(point.getKey() + ": " + point.getValue());
+        }
+    }
+
+
+    private void stopGame(){
+        this.scheduler.cancelTasks(this);
+        for(Item item: items){
+            item.remove();
+        }
+        Map<String, Integer> sorted = hashMapHelper.sortByValue(points, true);
+        getServer().broadcastMessage("" + ChatColor.GOLD + ChatColor.BOLD + "Final Result!");
+
+        int i = 1;
+        String[] col = {ChatColor.GOLD + "" + ChatColor.GOLD,
+                        ChatColor.GOLD + "",
+                        ChatColor.YELLOW + ""};
+        for (Map.Entry<String, Integer> point : sorted.entrySet()){
+            getServer().broadcastMessage(((i > col.length) ? "": col[i -1]) + i + ". " + point.getKey() + ": " + point.getValue());
+            i ++;
+        }
+
+        scoreboard.remove();
+
+        this.delay = 0L;
+
+        clearAll();
+    }
+
+
+    public void giveSpawnBasketToAllOnLinePlayers(){
+        for(Player p: getServer().getOnlinePlayers()){
+            p.sendMessage(ChatColor.GOLD + "" + ChatColor.BOLD + "How to Play: ");
+            p.sendMessage(ChatColor.BOLD + "Left click" + "" + "" +  ChatColor.WHITE +  " on the ground to Spawn your basket!");
+            p.sendMessage("Collect mobs to your hole as many as you can!");
+            p.sendMessage("Every time you collect a NEW type of mob you'll get 5 points, Otherwise you'll only get 1 point");
+            Location loc = p.getLocation().clone().add(0, 1, 0);
+            loc.getWorld().dropItemNaturally(loc, ItemHandler.getSpawnBasket());
+        }
+    }
+
+    private void initialize(){
+        scoreboard = new ScoreboardManager("mobHunt", ChatColor.GOLD + "" + ChatColor.BOLD + "Mob Hunt");
+        settings = new ConfigManager(this, "settings.yml");
+        this.basketLocs = new HashMap<>();
+        this.collectedType = new HashMap<>();
+        this.points = new HashMap<>();
+        this.locsInBasket = new HashMap<>();
+        this.tasks = new HashMap<>();
+        this.items = new ArrayList<>();
+        this.scheduler = getServer().getScheduler();
+        hashMapHelper = new HashMapHelper();
+    }
+
+    public void reset(){
+        this.scheduler.cancelTasks(this);
+
+        for(Item item: items){
+            item.remove();
+        }
+        clearAll();
+
+        scoreboard.remove();
+
+        this.delay = 0L;
+    }
+
+    private void clearAll(){
+        this.items.clear();
+        this.basketLocs.clear();
+        this.collectedType.clear();
+        this.locsInBasket.clear();
+        this.items.clear();
+        this.tasks.clear();
+        this.points.clear();
+    }
+
+
+
+    @Override
+    public void onEnable() {
+        initialize();
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @Override
+    public void onDisable() {
+        reset();
+    }
+
+    @SuppressWarnings("NullableProblems")
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if(command.getName().equalsIgnoreCase("MHDisable")){
+            reset();
+            return true;
+        }
+        else if(command.getName().equalsIgnoreCase("MHPoints")){
+            msgPoints();
+            return true;
+        }
+        else if(command.getName().equalsIgnoreCase("MHStart")){
+            if(args.length != 1){
+                sender.sendMessage(ChatColor.RED + "usage: /MHStart <5Minutes/10Minutes/20Minutes/30Minutes/45Minutes/1Hour/1Hour30Minutes/2Hour>");
+                return false;
+            }
+
+            long aMinute = 20*60;
+
+            switch (args[0]) {
+                case "5Minutes":
+                    this.delay = aMinute * 5L;
+                    break;
+                case "10Minutes":
+                    this.delay = aMinute * 10L;
+                    break;
+                case "20Minutes":
+                    this.delay = aMinute * 20L;
+                    break;
+                case "30Minutes":
+                    this.delay = aMinute * 30L;
+                    break;
+                case "45Minutes":
+                    this.delay = aMinute * 45L;
+                    break;
+                case "1Hour":
+                    this.delay = aMinute * 60L;
+                    break;
+                case "1Hour30Minutes":
+                    this.delay = aMinute * 90L;
+                    break;
+                case "2Hours":
+                    this.delay = aMinute * 120L;
+                    break;
+                default:
+                    return false;
+            }
+
+            for(Player player: getServer().getOnlinePlayers()){
+                scoreboard.create(player);
+                scoreboard.put(player.getName(), 0);
+            }
+
+            giveSpawnBasketToAllOnLinePlayers();
+            for(Player p: getServer().getOnlinePlayers()){
+                PlayerInventory inventory = p.getInventory();
+                if(!inventory.contains(Material.COMPASS)){
+                    inventory.addItem(new ItemStack(Material.COMPASS));
+                }
+            }
+            getServer().broadcastMessage("Mob Hunt started for " + (delay/20/60) + " Minutes!");
+
+            return true;
+        }else if(command.getName().equalsIgnoreCase("MHSetBasketSize")){
+            if(args.length != 1){
+                sender.sendMessage("use: /MHSetBasketSize <number>");
+                return false;
+            }
+
+            try {
+                int size = Integer.parseInt(args[0]);
+                settings.getConfig().set("basketSize", size);
+                settings.saveConfig();
+                return true;
+            }catch (Exception err){
+                sender.sendMessage(ChatColor.RED + "Please input a number.");
+                return false;
+            }
+        }
+        return false;
+    }
+
+    @SuppressWarnings("NullableProblems")
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+
+        List<String> arguments = new ArrayList<>();
+
+        if(command.getName().equalsIgnoreCase("MHStart")){
+            arguments.add("5Minutes");
+            arguments.add("10Minutes");
+            arguments.add("20Minutes");
+            arguments.add("30Minutes");
+            arguments.add("45Minutes");
+            arguments.add("1Hour");
+            arguments.add("1Hour30Minutes");
+            arguments.add("2Hours");
+            List<String> result = new ArrayList<>();
+            if(args.length == 1){
+                for(String a: arguments){
+                    if(a.toLowerCase().startsWith(args[0].toLowerCase())){
+                        result.add(a);
+                    }
+                }
+                return result;
+            }
+        }
+        return null;
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    @EventHandler
+    public void onClick(PlayerInteractEvent event){
+
+        if(event.getAction().equals(Action.LEFT_CLICK_BLOCK)) {
+
+            if (ItemHandler.isHolding(ItemHandler.getSpawnBasket(), event.getPlayer())) {
+                Location loc = event.getClickedBlock().getLocation();
+
+                while (!loc.getBlock().getType().isSolid()){
+                    loc.subtract(0, 1,0);
+                }
+
+                event.setCancelled(true);
+                spawnBasket(loc, Material.BEDROCK, settings.getConfig().getInt("basketSize"), event.getPlayer(), delay);
+                event.getPlayer().getInventory().remove(ItemHandler.getSpawnBasket());
+
+            }
+        }
+        else if(event.getAction().equals(Action.RIGHT_CLICK_BLOCK)){
+
+            if(ItemHandler.isHolding(ItemHandler.getSpawnBasket(), event.getPlayer()))
+                event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onPickUpItems(EntityPickupItemEvent event){
+        if(event.getEntity() instanceof Player){
+
+            for(Item i: items){
+
+                if(event.getItem().getItemStack().isSimilar(i.getItemStack()))
+                    event.setCancelled(true);
+            }
+        }
+    }
+
+    public void spawnBasket(Location loc, Material mat, int distance, final Player player, long delay) {
+
+        if (this.basketLocs.containsKey(player)) {
+            this.basketLocs.replace(player, loc);
+        } else {
+            this.basketLocs.put(player, loc);
+        }
+
+        Location centerXp = loc.clone().add(distance, -distance, 0.0D);
+        Location centerXn = loc.clone().add(-distance, -distance, 0.0D);
+        Location centerYn = loc.clone().add(0.0D, -distance, 0.0D);
+        Location centerZp = loc.clone().add(0.0D, -distance, distance);
+        Location centerZn = loc.clone().add(0.0D, -distance, -distance);
+
+        List<Location> locs1 = new ArrayList<>();
+
+        for (double i = centerXn.getX() + 1; i < centerXp.getX(); i+=1) {
+            for (double j = centerYn.getY() + 1; j <= loc.getY(); j+=1) {
+                for (double k = centerZn.getZ() + 1; k < centerZp.getZ(); k+=1) {
+                    Location loc1 = new Location(loc.getWorld(), i, j, k);
+                    Block b = loc1.getBlock();
+                    b.setType(Material.AIR);
+                    locs1.add(b.getLocation());
+                }
+            }
+        }
+
+        this.locsInBasket.put(player, locs1);
+
+        double horizon;
+        for (horizon = centerXn.getZ() - distance; horizon < centerXn.getZ() + distance; horizon+=1) {
+            double vertical;
+            for (vertical = centerYn.getY(); vertical <= loc.getY(); vertical+=1) {
+                Location curr = new Location(loc.getWorld(), centerXn.getX(), vertical, horizon);
+                curr.getBlock().setType(mat);
+            }
+        }
+        for (horizon = centerXp.getZ() + distance; horizon > centerXp.getZ() - distance; horizon-=1) {
+            double vertical;
+            for (vertical = centerYn.getY(); vertical <= loc.getY(); vertical+=1) {
+                Location curr = new Location(loc.getWorld(), centerXp.getX(), vertical, horizon);
+                curr.getBlock().setType(mat);
+            }
+        }
+        for (horizon = centerZn.getX() + distance; horizon > centerZn.getX() - distance; horizon-=1) {
+            double vertical;
+            for (vertical = centerYn.getY(); vertical <= loc.getY(); vertical+=1) {
+                Location curr = new Location(loc.getWorld(), horizon, vertical, centerZn.getZ());
+                curr.getBlock().setType(mat);
+            }
+        }
+        for (horizon = centerZp.getX() - distance; horizon < centerZp.getX() + distance; horizon+=1) {
+            double vertical;
+            for (vertical = centerYn.getY(); vertical <= loc.getY(); vertical+=1) {
+                Location curr = new Location(loc.getWorld(), horizon, vertical, centerZp.getZ());
+                curr.getBlock().setType(mat);
+            }
+        }
+        double x;
+        for (x = centerYn.getX() - distance; x <= centerYn.getX() + distance; x+=1) {
+            double z;
+            for (z = centerYn.getZ() - distance; z <= centerYn.getZ() + distance; z+=1) {
+                Location curr = new Location(loc.getWorld(), x, centerYn.getY(), z);
+                curr.getBlock().setType(mat);
+            }
+        }
+
+        // LADDER
+        for(double y = centerYn.getY() + 1; y <= loc.getY(); y++){
+            Location loc2 = new Location(loc.getWorld(), loc.getX(), y, centerZp.getZ() - (distance - 1));
+            loc2.getBlock().setType(Material.LADDER);
+        }
+
+        ItemStack is = ItemHandler.getRandomDiamondItems();
+        final Item item = player.getWorld().dropItemNaturally(loc.clone().add(0.0D, 2.0D, 0.0D), is);
+        this.items.add(item);
+
+        player.setCompassTarget(basketLocs.get(player));
+
+        hashMapHelper.putOrReplace(this.tasks, player, this.scheduler.scheduleSyncRepeatingTask(this, new Runnable() {
+            int c = 0;
+
+            public void run() {
+                if (c > 2) {
+                    if (player.getLocation().distance(basketLocs.get(player)) < 10.0D) {
+                        List<Entity> nearByE = player.getNearbyEntities(5.0D, 5.0D, 5.0D);
+                        for (Entity e : nearByE) {
+                            if (!e.getType().name().contains("DROPPED")
+                                    && !(e instanceof Player)
+                                    && !e.getType().name().contains("BOAT")
+                                    && !e.getType().name().contains("MINECART")) {
+                                Location eLoc = e.getLocation().clone();
+                                eLoc.setX(eLoc.getBlock().getX());
+                                eLoc.setY(eLoc.getBlock().getY());
+                                eLoc.setZ(eLoc.getBlock().getZ());
+                                for (Location loc : locsInBasket.get(player)) {
+                                    if (loc.getX() == eLoc.getX() && loc.getY() == eLoc.getY() && loc.getZ() == eLoc.getZ()) {
+                                        int increment = getPointIncrement(player, e.getType());
+                                        e.remove();
+                                        increment(points, player.getName(), increment);
+                                        getServer().broadcastMessage("" + ChatColor.GREEN + player.getName() +
+                                                " collected a "  + ((increment == 5) ? ("" + ChatColor.BOLD + "new mob") : "mob") +
+                                                ". + " + increment +  " point" + ((increment > 1) ? "s": ""));
+                                        scoreboard.put(player.getName(), points.get(player.getName()));
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                c++;
+                item.setGravity(false);
+                item.setVelocity(new Vector(0, 0, 0));
+            }
+        }, 0L, 20L), false);
+        this.scheduler.scheduleSyncDelayedTask(this, this::countDown, delay);
+    }
+
+
+
+    public int getPointIncrement(Player player, EntityType et){
+
+        // If the player collected a 'NEW' type of mobs, they'll get 5 points, otherwise they'll get 1 point
+        List<EntityType> et1 = new ArrayList<>();
+        et1.add(et);
+        if(collectedType.get(player) != null) {
+            if(!collectedType.get(player).contains(et)){
+                if(collectedType.containsKey(player)){
+                    List<EntityType> ct = collectedType.get(player);
+                    ct.add(et);
+                    collectedType.replace(player, ct);
+                }else{
+                    collectedType.put(player, et1);
+                }
+                return 5;
+            }else {
+                return 1;
+            }
+        }else{
+            collectedType.put(player, et1);
+            return 5;
+        }
+    }
+
+}
